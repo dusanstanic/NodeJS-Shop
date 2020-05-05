0893729372733eddd2c7efcc1e2bb91ef4e91232
Dusan <dusan.stanic97@hotmail.com>
2020-05-05 16:09:52 +0200

    Add updating posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+
 const { validationResult } = require("express-validator/check");
 
 const Post = require("../models/post");
@@ -30,7 +33,6 @@ exports.createPost = (req, res, next) => {
     throw error;
   }
   const imageUrl = req.file.path;
-  console.log(imageUrl);
   const image = imageUrl.split("\\");
   const title = req.body.title;
   const content = req.body.content;
@@ -73,4 +75,57 @@ exports.getPost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incorrect.");
+    error.statusCode = 422;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  console.log("1 " + imageUrl);
+  if (req.file) {
+    const image = req.file.path.split("\\");
+    console.log("2 " + image);
+    imageUrl = image[0] + "/" + image[1];
+  }
+  if (!imageUrl) {
+    const error = new Error("No file picked.");
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Post updated!", post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
